1c582f5fe78c6b694b05c39bf9492787efe21d5a
nanaaks <nanaaks@outlook.com>
2025-11-26 22:42:46 -0500

    Added declaration to js files

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+/******************************************************************************
+* ITE5315 – Project
+* I declare that this project is my own work in accordance with Humber Academic Policy.
+* No part of this project has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+* Group Member Names: Daniel Tarhembe, Jeffrey Lamptey, Nana Sackey
+* Student IDs: n01719446, n01675664, n01700360
+* Date: 2025-11-26
+******************************************************************************/
+
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
